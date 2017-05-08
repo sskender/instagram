36084811c94f1f6c74838b4e2f8a1a40f305a383
sskender <sskender>
2017-05-08 23:19:12 +0200

    MySQL configuration script

---
+++ instagram/config.sql
@@ -0,0 +1,52 @@
+/**
+ *  MySQL DATABASE SETUP
+ *
+ *
+ *  Initialize file like this:
+ *
+ *  mysql -u <username> -p < config.sql
+ *  Type in password
+ *
+ *  DONE!
+ */
+
+
+
+-- DATABASE
+CREATE DATABASE instagram;
+USE instagram;
+
+
+
+-- USERS TABLE
+CREATE TABLE users (
+    user_id         INTEGER UNSIGNED    NOT NULL    AUTO_INCREMENT  PRIMARY KEY,
+    username        VARCHAR(30)         NOT NULL,
+    password        VARCHAR(255)        NOT NULL,
+    email           VARCHAR(255)        NOT NULL
+);
+
+
+-- FOLLOWS TABLE
+CREATE TABLE follows (
+    follow_id       INTEGER UNSIGNED    NOT NULL    AUTO_INCREMENT  PRIMARY KEY,
+    id_from         INTEGER UNSIGNED    NOT NULL,
+    id_to           INTEGER UNSIGNED    NOT NULL
+);
+
+
+-- PROFILE PHOTOS TABLE
+CREATE TABLE photos (
+    photo_id        INTEGER UNSIGNED    NOT NULL    AUTO_INCREMENT  PRIMARY KEY,
+    user_id         INTEGER UNSIGNED    NOT NULL,
+    photo_hash      VARCHAR(50)         NOT NULL
+);
+
+
+-- UPLOADS TABLE
+CREATE TABLE uploads (
+    photo_id        INTEGER UNSIGNED    NOT NULL    AUTO_INCREMENT  PRIMARY KEY,
+    user_id         INTEGER UNSIGNED    NOT NULL,
+    photo_hash      VARCHAR(50)         NOT NULL,
+    timestamp       DATETIME            NOT NULL    DEFAULT NOW(),
+);

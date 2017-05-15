080bd3d333eb61345d34f56cf3ca0225d1f46923
sskender <sskender>
2017-05-15 04:08:35 +0200

    Design update

--- instagram/resources/lib/update.profile.php
+++ instagram/resources/lib/update.profile.php
@@ -81,8 +81,12 @@ if (isset($_POST["newemail"])) {
 ?>
                 <section>
                     <form method="post" class="contact-form" action="upload.php" enctype="multipart/form-data">
-                        <input type="file" name="profile-upload" id="fileToUpload">
-                        <input type="submit" value="Upload Image" name="submit">
+                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                            <div class="btn dark-blue-bordered-btn normal-btn" name="profile-upload" id="profile-update-button">
+                                Update photo
+                                <input name="profile-upload" type="file" style="opacity:0" onchange="this.form.submit()">
+                            </div>
+                        </div>
                     </form>
                 </section>
                 <br>
